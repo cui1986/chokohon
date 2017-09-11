6277752ae6b9ff3c0e6bbc9810a193d30e8422f6
whb1324 <whb1324@gmail.com>
2017-09-11 19:12:50 +0900

    wang_searchform and zhou_searchform

---
+++ src/Controller/BooksController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use App\Form\SearchBookForm;
+use Cake\I18n\Date;
+use Cake\ORM\TableRegistry;
+
+
+/**
+ * Books Controller
+ *
+ * @property \App\Model\Table\BooksTable $Books
+ *
+ * @method \App\Model\Entity\Book[] paginate($object = null, array $settings = [])
+ */
+class BooksController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+
+    }
+    public function view($id = null)
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function edit($id = null)
+    {
+
+    }
+
+}
