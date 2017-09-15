a2704cf7ddd2b832e78a75fa8be68abdcac1157c
whb1324 <31403562+whb1324@users.noreply.github.com>
2017-09-15 11:22:35 +0900

    Add files via upload
    merukaritable

--- src/Model/Table/MerukariTable.php
+++ src/Model/Table/MerukariTable.php
@@ -10,59 +10,71 @@ use Cake\ORM\TableRegistry;
 
 class MerukariTable extends Table implements BookInfo
 {
+
   public function initialize(array $config)
   {
-
     parent::initialize($config);
     $this->setTable('merukari_rules');
 
     $this->hasOne('Books', [
-        'foreignKey' => 'id',
-        'joinType' => 'INNER'
+      'foreignKey' => 'book_id',
+      'joinType' => 'INNER'
     ]);
   }
-
   function get_books(int $book_id){
+
+    $book_rules = TableRegistry::get('merukari_rules');
+
+    $rule = $book_rules->get($book_id);
     $data_array = array();
-    $page_number = 1;
-    $keyword = "cakephp";
-    $url =  "https://www.mercari.com/jp/search/?sort_order=&keyword=cakephp&category_root=5&category_child=72&category_grand_child%5B668%5D=1&category_grand_child%5B669%5D=1&category_grand_child%5B670%5D=1&category_grand_child%5B671%5D=1&category_grand_child%5B672%5D=1&category_grand_child%5B673%5D=1&category_grand_child%5B674%5D=1&category_grand_child%5B675%5D=1&category_grand_child%5B676%5D=1&category_grand_child%5B677%5D=1&category_grand_child%5B678%5D=1&category_grand_child%5B1124%5D=1&category_grand_child%5B679%5D=1&brand_name=&brand_id=&size_group=&price_min=&price_max=&condition_all=1&item_condition_id%5B1%5D=1&item_condition_id%5B2%5D=1&item_condition_id%5B3%5D=1&item_condition_id%5B4%5D=1&item_condition_id%5B5%5D=1&item_condition_id%5B6%5D=1&shipping_payer_all=1&shipping_payer_id%5B1%5D=1&shipping_payer_id%5B2%5D=1&status_all=1&status_on_sale=1&status_trading_sold_out=1";
 
-    $html = file_get_html( $url );
+    //取检索条件
+    $key_words = $rule["key_words"];
+    $category_id = $rule["category_id"];
+    $book_status = $rule["book_status"];
+    $delivery_id = $rule["delivery_id"];
+    $on_sale = $rule["on_sale"];
+    $sold_out = $rule["sold_out"];
 
-    $results = $html->find( '.items-box' );
+    //解析url
+    $url =  "https://www.mercari.com/jp/search/?sort_order=&keyword={$key_words}&category_root=5&category_child=72&category_grand_child[{$category_id}]=1&brand_name=&brand_id=&size_group=&price_min=&price_max=&item_condition_id[{$book_status}]=1&shipping_payer_id[{$delivery_id}]=1&status_on_sale={$on_sale}&status_trading_sold_out={$sold_out}";
 
+    $html = file_get_html($url);
 
-    foreach ($results as $result) {
-        $temp_array = array();
+    $result_null = $html->find(".search-result-description");
+    if(count($result_null) > 0){
+        return $data_array;
+    }else {
+        $results = $html->find( '.items-box' );
 
-        $img_src_name = "data-src";
-        $temp_array['buy_link'] = $result->children[0]->href;
-        $temp_array['book_img'] =  $result->children[0]->children[0]->children[0]->$img_src_name;
-        if(isset($result->children[0]->children[0]->children[1]->children[0])){
-            $temp_array['sale_status'] = $result->children[0]->children[0]->children[1]->children[0]->plaintext;
-        }else {
-          $temp_array['sale_status'] = "";
+        foreach ($results as $result) {
+            $temp_array = array();
+
+            $img_src_name = "data-src";
+            //购买网址
+            $temp_array['buy_link'] = $result->children[0]->href;
+            //图像url
+            $temp_array['book_img'] =  $result->children[0]->children[0]->children[0]->$img_src_name;
+            //贩卖情况
+            if(isset($result->children[0]->children[0]->children[1]->children[0])){
+              $temp_array['sale_status'] = $result->children[0]->children[0]->children[1]->children[0]->plaintext;
+            }else {
+              $temp_array['sale_status'] = "";
+            }
+            //书名
+            $temp_array['book_name'] =  $result->children[0]->children[1]->children[0]->plaintext;
+            //价格
+            $temp_array['price'] = $result->children[0]->children[1]->children[1]->children[0]->plaintext;
+
+            $data_array[] = $temp_array;
         }
-        $temp_array['book_name'] =  $result->children[0]->children[1]->children[0]->plaintext;
-        $temp_array['price'] = $result->children[0]->children[1]->children[1]->children[0]->plaintext;
 
-        $data_array[] = $temp_array;
+        $html->clear();
+
+        return $data_array;
     }
 
-    $html->clear();
-    return $data_array;
-  }
-  public function initialize(array $config)
-  {
-
-    parent::initialize($config);
-    $this->setTable('merukari_rules');
-
-    $this->hasOne('Books', [
-        'foreignKey' => 'id',
-        'joinType' => 'INNER'
-    ]);
+    $result_null->clear();
   }
 }
 ?>
